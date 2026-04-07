64e2b17683e7addf450c035c6048c33fd7778823
siddhartha-pant <iamsiddhartha23@gmail.com>
2026-04-07 10:24:32 +0530

    Update nutrition.routes.ts

--- src/routes/nutrition.routes.ts
+++ src/routes/nutrition.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { logMeal, getMeals } from "../controllers/meal.controller";
 import { logWater, getWaterLogs } from "../controllers/water.controller";
-import { saveDiaryEntry, getDiaryEntries } from "../controllers/diary.controller";
+import { saveDiaryEntry, getDiaryEntries, updateDiaryEntry } from "../controllers/diary.controller";
 import { foodSearch } from "../controllers/foodSearch.controller";
 import auth from "../middlewares/auth";
 import { validateMeal } from "../middlewares/validation";
@@ -16,10 +16,14 @@ const router = Router();
 
 router.post("/meal", auth, validateMeal, asyncHandler(logMeal));
 router.get("/meal", auth, asyncHandler(getMeals));
+
 router.post("/water", auth, asyncHandler(logWater));
 router.get("/water", auth, asyncHandler(getWaterLogs));
+
 router.post("/diary", auth, asyncHandler(saveDiaryEntry));
 router.get("/diary", auth, asyncHandler(getDiaryEntries));
+router.patch("/diary/:id", auth, asyncHandler(updateDiaryEntry)); // ← new
+
 router.get("/food-search", asyncHandler(foodSearch));
 
 export default router;
